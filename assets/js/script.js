4f6fc0be18adf286dce2e6391dbd092285a8accb
Adrien Trompette <adrientrompette1998@gmail.com>
2022-10-06 18:23:14 +0200

    Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,32 @@ let items = document.querySelectorAll('.item');
 let clearBtn = document.querySelector('.clear-btn');
 let count = document.querySelector('.count');
 let unreads = document.getElementsByClassName('unread');
+let unreadIcon = document.querySelectorAll('unread-btn');
+
+count.textContent = unreads.length;
 
 // Add or remove unread classes to items
 items.forEach(i => {
     i.addEventListener('click', () => {
-        i.classList.toggle('unread');
+        if(i.classList.contains('unread')) {
+            i.classList.remove('unread');
+        };
+
+        count.textContent = unreads.length;
     });
 });
-
 
 // Remove unread class when clicking on the clear button
 clearBtn.addEventListener('click', () => {
     items.forEach(i => {
         i.classList.remove('unread');
     });
+
+    count.textContent = 0;
 });
+
+
+
 
 
 
